40bcb2bc07c75ccf7d07df25981d831c85dd5381
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-14 12:57:45 +0100

    Added hobbies and emplyment js files

---
+++ src/assets/data/employment.js
@@ -0,0 +1,17 @@
+const employment = [
+  {
+    employerName: "Focus Group",
+    jobTitle: "Resource Co-Ordinator",
+    timeInJob: "Jan 2020 - April 2020",
+    employmentType: "Contract",
+    responsibilities: "",
+  },
+
+  {
+    employerName: "",
+    jobTitle: "",
+    timeInJob: "",
+    employmentType: "",
+    responsibilities: "",
+  },
+];
---
+++ src/assets/data/hobbies.js
@@ -0,0 +1,3 @@
+const hobbies = [
+  { hobby1: "Sailing", hobby2: "Skiing", hobby3: "Fencing", hobby4: "Archery" },
+];
