73b82f6abcaee7d7fb9a2d36f8d3d07b835029be
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-14 13:18:02 +0100

    Updated emplyment js file

--- src/assets/data/employment.js
+++ src/assets/data/employment.js
@@ -1,17 +1,71 @@
 const employment = [
   {
     employerName: "Focus Group",
-    jobTitle: "Resource Co-Ordinator",
+    jobTitle: "Resource Coordinator",
     timeInJob: "Jan 2020 - April 2020",
     employmentType: "Contract",
-    responsibilities: "",
   },
 
   {
-    employerName: "",
-    jobTitle: "",
-    timeInJob: "",
-    employmentType: "",
-    responsibilities: "",
+    employerName: "OCSL",
+    jobTitle: "Project Manager",
+    timeInJob: "Nov 2013 - Dec 2015",
+    employmentType: "Permanent",
+  },
+
+  {
+    employerName: "RBS Global Banking & Markets",
+    jobTitle: "Business Analyst",
+    timeInJob: "Feb 2010 - May 2011",
+    employmentType: "Contract",
+  },
+
+  {
+    employerName: "Claires",
+    jobTitle: "Business Analyst",
+    timeInJob: "Jan 2009 - Feb 2010",
+    employmentType: "Contract",
+  },
+
+  {
+    employerName: "Reading Room",
+    jobTitle: "Project Manager",
+    timeInJob: "May 2008 - June 2008",
+    employmentType: "Contract",
+  },
+
+  {
+    employerName: "Benelux Playboy",
+    jobTitle: "Project Manager",
+    timeInJob: "Jan 2006 - April 2007",
+    employmentType: "Contract",
+  },
+
+  {
+    employerName: "Inmedia Communications",
+    jobTitle: "Broadcast Transmission Controller",
+    timeInJob: "Sept 2001 - Dec 2005",
+    employmentType: "Permanent",
+  },
+
+  {
+    employerName: "Whereits.At",
+    jobTitle: "Broadcast Transmission Controller",
+    timeInJob: "April 2001 - August 2001",
+    employmentType: "Permanent",
+  },
+
+  {
+    employerName: "BBC News",
+    jobTitle: "News Editing Assistant",
+    timeInJob: "Jan 2000 - March 2001",
+    employmentType: "Permanent",
+  },
+
+  {
+    employerName: "Hartswood Films",
+    jobTitle: "Lighting technician/Camera Crew",
+    timeInJob: "April 1999 - November 1999",
+    employmentType: "Permanent",
   },
 ];
