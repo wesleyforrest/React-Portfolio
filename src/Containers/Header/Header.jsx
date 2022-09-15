bb5532e567c26ac1bdd126aaf22ead791e06c688
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-15 11:39:16 +0100

    Styling to header

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -13,6 +13,15 @@ const Header = () => {
         <h1 className="header-section__job-title">Junior Software Developer</h1>
         <div className="header-section__underline-gold">________</div>
       </div>
+      <div className="header-section__course-summary">
+        <p className="header-section__course-summary-text">
+          I recently applied for and got accepted to join the nology 12 week
+          intensive full stack consultancy coding bootcamp program. The course
+          took me from having virtually no coding knowledge through modules in
+          html, scss, javaScript, React and Java. This portfolio showcases the
+          skills I have learned during this process.{" "}
+        </p>
+      </div>
     </div>
   );
 };
