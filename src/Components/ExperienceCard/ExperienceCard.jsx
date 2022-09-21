eec06199187f5aa758873020fb17d05e5f409061
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-21 13:12:18 +0100

    Created experience section

---
+++ src/Components/ExperienceCard/ExperienceCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ExperienceCard.scss";
+
+const ExperienceCard = () => {
+  return (
+    <div>
+      <div className="experience">
+        <h2 className="experience-section__header underline">Experience</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceCard;
