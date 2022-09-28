1765f2298563068fc3a7963a1d43a83b374a82bd
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-28 11:28:12 +0100

    Added linked to footer icons

--- src/Containers/Footer/Footer.jsx
+++ src/Containers/Footer/Footer.jsx
@@ -7,15 +7,33 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__icon-container">
-        <div className=" footer__icon footer__icon-linkedin">
-          <BsLinkedin size={30} />
-        </div>
-        <div className="footer__icon footer__icon-github">
-          <BsGithub size={30} />
-        </div>
-        <div className="footer__icon footer__icon-cv">
-          <h2>CV</h2>
-        </div>
+        <a
+          href={"https://www.linkedin.com/in/wesley-forrest-6a846b15/"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className=" footer__icon footer__icon-linkedin">
+            <BsLinkedin size={30} />
+          </div>
+        </a>
+        <a
+          href={"https://github.com/wesleyforrest"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="footer__icon footer__icon-github">
+            <BsGithub size={30} />
+          </div>
+        </a>
+        <a
+          href={"./Images/Wesley Forrest CV 2022.pdf"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="footer__icon footer__icon-cv">
+            <h2>CV</h2>
+          </div>
+        </a>
       </div>
     </div>
   );
--- src/assets/data/dist/hobbies.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var hobbies = [{
-  hobby1: "Sailing",
-  hobby2: "Skiing",
-  hobby3: "Fencing",
-  hobby4: "Archery"
-}];
