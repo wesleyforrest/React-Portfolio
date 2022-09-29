43b37d3b95582c5eb75838634e073b551b011056
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-09-29 15:17:46 +0100

    Working contact form

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -4,13 +4,32 @@ import "./ContactForm.scss";
 const ContactForm = () => {
   return (
     <div className="contact-form-container">
-      <form className="contact-form">
+      <form
+        className="contact-form"
+        action="https://formsubmit.co/wesleyforrest@me.com"
+        method="POST"
+      >
         <label className="contact-form__name-label">Name</label>
-        <input className="contact-form__name-input" type="text" />
+        <input
+          className="contact-form__name-input"
+          type="text"
+          name="name"
+          required
+        />
         <label className="contact-form__email-label">Email</label>
-        <input className="contact-form__email-input" type="text" />
+        <input
+          className="contact-form__email-input"
+          type="email"
+          name="email"
+          required
+        />
         <label className="contact-form__message-label">Comment</label>
-        <input className="contact-form__message-input" type="text" />
+        <input
+          className="contact-form__message-input"
+          type="text"
+          name="message"
+          required
+        />
         <div className="contact-form__button-container">
           <input
             className="contact-form__submit-button"
